6df49219d7b1655415f9207dc1f7f597729fa5af
ANSHUMANMOREY7 <anshumanmorey2@gmail.com>
2025-05-03 20:35:11 +0530

    using innerHTML element digits are shown on the clocks screem

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ function loadclock() {
     const minuteelement = document.getElementById('minutes')
     const secondelement=document.getElementById('seconds') 
     
-    let date =new date() ;
+    let date = new Date() ;
     let hour = date.getHours();
     hours= hours > 12 ? hours - 12 : hours ;
     let minutes= date.getMinutes();
     let seconds = date.getSeconds();
     const AmPm = hours >= 12 ? 'PM' : 'AM'; 
+
+    hourelement.innerHTML= hour ;
+    minuteelement.innerHTML=minutes;
+    secondelement.innerHTML=seconds;
+
 }
+loadclock();
