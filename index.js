7dc447786665d0a4fedf5f8ac02705811962da85
ANSHUMANMOREY7 <anshumanmorey2@gmail.com>
2025-05-03 20:29:50 +0530

    id's accesed using id

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function loadclock() {
-
-    }
+    const hourelement =document.getElementById('hours')
+    const minuteelement = document.getElementById('minutes')
+    const secondelement=document.getElementById('seconds') 
+    
+ 
+}
