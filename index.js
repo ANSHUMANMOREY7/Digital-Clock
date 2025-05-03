764f26218a82ea6e76d8af33df026430d455b821
ANSHUMANMOREY7 <anshumanmorey2@gmail.com>
2025-05-03 20:30:52 +0530

    date hour seconds found

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function loadclock() {
     const minuteelement = document.getElementById('minutes')
     const secondelement=document.getElementById('seconds') 
     
- 
+    let date =new date() ;
+    let hour = date.getHours();
+    hours= hours > 12 ? hours - 12 : hours ;
+    let minutes= date.getMinutes();
+    let seconds = date.getSeconds();
+    const AmPm = hours >= 12 ? 'PM' : 'AM'; 
 }
