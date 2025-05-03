12cc9435da469de1d4ea3dab2744ef7e339fbfa6
ANSHUMANMOREY7 <anshumanmorey2@gmail.com>
2025-05-03 21:08:39 +0530

    ampm added

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ function loadclock() {
     const hourelement = document.getElementById('hours')
     const minuteelement = document.getElementById('minutes')
     const secondelement = document.getElementById('seconds')
-
+    const ampm =document.getElementById("ampm")
     let date = new Date();
     let hour = date.getHours();
     hour = hour > 12 ? hour - 12 : hour;
     hour = hour < 10 ? "0" + hour : hour;
     let minutes = date.getMinutes();
-    minutes = minutes < 10 ? "0" + minutes : minutes;
+    minutes = minutes <= 10 ? "0" + minutes : minutes;
     let seconds = date.getSeconds();
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    let AmPmours = AmPm > 12 ? 'PM' : 'AM';
-
-
+    seconds = seconds <= 9 ? "0" + seconds : seconds;
+    
     hourelement.innerHTML = hour;
     minuteelement.innerHTML = minutes;
     secondelement.innerHTML = seconds;
-    AmPm.innerHTML =
+    ampm.innerText = date.getHours() >= 12 ? "PM" : "AM";
+
 
 }
 setInterval(loadclock, 1000);
